3be11e9ddeb338ab295c241ce8948855399836df
Moin uddin <moincse022@gmail.com>
2024-09-02 17:47:56 +0600

    find the singal card

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b2nov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,6 +37,14 @@ async function run() {
       const result = await doctorCollection.find({}).toArray();
       res.send(result);
     });
+
+    app.get("/getDoctor/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await doctorCollection.findOne(query);
+      res.send(result);
+      
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
